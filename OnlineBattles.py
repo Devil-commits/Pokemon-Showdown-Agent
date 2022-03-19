207f88ba8487d616406e28a945d5f742a50c8225
Devil-commits <83263569+Devil-commits@users.noreply.github.com>
2022-03-19 11:01:04 +0530

    Update OnlineBattles.py

--- OnlineBattles.py
+++ OnlineBattles.py
@@ -15,6 +15,65 @@ from MiniMax import MinimaxPlayer
 from poke_env.player.baselines import SimpleHeuristicsPlayer
 
 async def main(): 
+    team_1 = """
+Pelipper @ Damp Rock  
+Ability: Drizzle  
+EVs: 248 HP / 8 Def / 252 SpD  
+Calm Nature  
+IVs: 0 Spe  
+- Scald  
+- Defog  
+- U-turn  
+- Roost  
+
+Barraskewda @ Choice Band  
+Ability: Swift Swim  
+EVs: 252 Atk / 4 SpD / 252 Spe  
+Adamant Nature  
+- Flip Turn  
+- Liquidation  
+- Crunch  
+- Close Combat  
+
+Porygon-Z @ Choice Specs  
+Ability: Download  
+EVs: 252 SpA / 4 SpD / 252 Spe  
+Modest Nature  
+IVs: 0 Atk  
+- Shadow Ball  
+- Ice Beam  
+- Tri Attack  
+- Psyshock  
+
+Ferrothorn @ Rocky Helmet  
+Ability: Iron Barbs  
+EVs: 248 HP / 252 Def / 8 Spe  
+Impish Nature  
+- Stealth Rock  
+- Body Press  
+- Knock Off  
+- Leech Seed  
+
+Tapu Lele @ Choice Scarf  
+Ability: Psychic Surge  
+EVs: 252 SpA / 4 SpD / 252 Spe  
+Modest Nature  
+IVs: 0 Atk  
+- Future Sight  
+- Moonblast  
+- Thunder  
+- Psyshock  
+
+Seismitoad @ Choice Specs  
+Ability: Swift Swim  
+EVs: 252 SpA / 4 SpD / 252 Spe  
+Timid Nature  
+IVs: 0 Atk  
+- Grass Knot  
+- Earth Power  
+- Icy Wind  
+- Surf  
+"""
     
     #print("Random Player:")
 
@@ -50,6 +109,7 @@ async def main():
     print("Minimax Player")
 
     player = MinimaxPlayer(
+           battle_format="gen8ou", team=team, max_concurrent_battles=1,            
            player_configuration=PlayerConfiguration("LeadNitrate", "anchit@123"),
            server_configuration=ShowdownServerConfiguration
     )
